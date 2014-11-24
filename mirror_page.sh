1c1c02b3a6c97154b33a7e0078042ace21ebdb96
Wolfgang Dinter <info@werkstatt-fuer-gitarrenbau.de>
2014-11-24 19:44:33 +0100

    replaced rsync with cp

--- mirror_page.sh
+++ mirror_page.sh
@@ -3,4 +3,4 @@ DIR=`dirname "$0"`
 OUTDIR="$DIR/htdocs"
 mkdir -p $OUTDIR
 wget -m -p -k -nH -P "$OUTDIR" http://localhost:8888
-rsync -av "$DIR/static/js" "$OUTDIR/static"
+cp -R "$DIR/static" "$OUTDIR/"
