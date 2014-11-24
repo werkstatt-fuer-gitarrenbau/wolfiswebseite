cd472642af31471df3ca186a9beb0d47efbcc8a2
Wolfgang Dinter <info@werkstatt-fuer-gitarrenbau.de>
2014-11-24 19:45:16 +0100

    automatically remove outdir in mirror_page

--- mirror_page.sh
+++ mirror_page.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 DIR=`dirname "$0"`
 OUTDIR="$DIR/htdocs"
+if [ -d "$OUTDIR" ]; then
+rm -R $OUTDIR
+fi
 mkdir -p $OUTDIR
 wget -m -p -k -nH -P "$OUTDIR" http://localhost:8888
 cp -R "$DIR/static" "$OUTDIR/"
