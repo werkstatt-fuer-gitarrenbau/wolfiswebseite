1100d592efe892e1dbd6bfcaac6a918353cd6fee
Tobias Kölling <tobi@die70.de>
2015-10-24 16:19:33 +0200

    added basic guitar pages

--- server.py
+++ server.py
@@ -3,6 +3,10 @@ import os
 import tornado.ioloop
 import tornado.web
 import scss
+import yaml
+import glob
+import StringIO
+from PIL import Image
 
 scss.config.PROJECT_ROOT = os.path.join(os.path.dirname(__file__), "templates", "scss")
 
@@ -15,13 +19,42 @@ GITARREN_NAV = [('Neubau', 'neubau.html'),
                 ('Gebrauchtes', 'gebrauchtes.html'),
                 ('Reparaturen', 'reparaturen.html'),
                 ('Restauration', 'restauration.html'),]
+
+def load_guitar_info(folder):
+    with open(os.path.join(folder, 'gitarren.yaml'),
+              'r') as guitar_list_file:
+        guitar_list = yaml.load(guitar_list_file)
+    def load_single_guitar_info(guitar):
+        guitars_folder = os.path.join(folder, 'gitarren')
+        guitar_folder = os.path.join(guitars_folder, guitar)
+        try:
+            with open(os.path.join(guitar_folder,
+                                   '%s.yaml' % guitar)) as guitar_info:
+                res = yaml.load(guitar_info)
+        except IOError:
+            res = {'name': guitar}
+        res['folder'] = guitar
+        prefix_len = len(guitars_folder + os.path.sep)
+        images = sorted(glob.glob(os.path.join(guitar_folder, '*.jpg')))
+        res['images'] = [path[prefix_len:] for path in images]
+        res['images_small'] = [name.replace('.jpg', '.300.jpg')
+                               for name in res['images']]
+        return res
+    return map(load_single_guitar_info, guitar_list)
+
 class BaseHandler(tornado.web.RequestHandler):
+    def initialize(self):
+        self.guitar_info = \
+            load_guitar_info(os.path.join(self.settings["data_path"],
+                                          'gitarren_neubau'))
+
     def render(self, *args, **kwargs):
         self.set_header('Content-Type', 'text/html; charset=utf-8')
         self.set_header('Cache-Control', 'must-revalidate; max-age=0')
         kwargs['css_dir'] = ""
         kwargs['navitems'] = NAV
         kwargs['gitarren_nav'] = GITARREN_NAV
+        kwargs['guitar_info'] = self.guitar_info
         super(BaseHandler, self).render(*args, **kwargs)
 
 class MainHandler(BaseHandler):
@@ -29,6 +62,34 @@ class MainHandler(BaseHandler):
         if page == "home.html":
             page = "base.html"
         self.render(page)
+
+class GitarrenNeubauHandler(BaseHandler):
+    def get(self, guitar):
+        guitar_by_folder = {g['folder']:g for g in self.guitar_info}
+        guitar = guitar_by_folder[guitar]
+        self.render("gitarren_neubau.html",
+                guitar=guitar)
+
+class GitarrenNeubauImageHandler(tornado.web.RequestHandler):
+    def get(self, image, size=None):
+        if size is None:
+            def resize(fil):
+                return fil
+        else:
+            size = int(size)
+            def resize(fil):
+                sio = StringIO.StringIO()
+                img = Image.open(fil)
+                img.thumbnail((size, size), Image.ANTIALIAS)
+                img.save(sio, "JPEG")
+                sio.seek(0)
+                return sio
+        self.set_header('Content-Type', 'image/jpeg; charset=utf-8')
+        with open(os.path.join(self.settings['data_path'],
+                               'gitarren_neubau',
+                               'gitarren',
+                               '%s.jpg' % image)) as img_file:
+            self.write(resize(img_file).read())
 
 class StartHandler(tornado.web.RequestHandler):
     def get(self, page="start.html"):
@@ -53,6 +114,8 @@ class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
                 (r"/", StartHandler),
+                (r"/gitarren/neubau/(.+)\.html", GitarrenNeubauHandler),
+                (r"/gitarren/neubau/(?P<image>[^\.]+)(?:\.(?P<size>[0-9]+))?\.jpg", GitarrenNeubauImageHandler),
                 (r"/([^/\\]+\.html)", MainHandler),
                 (r"/(gitarren/[^/\\]+\.html)", MainHandler),
                 (r"/stylesheet.css", StylesheetHandler),
@@ -60,6 +123,7 @@ class Application(tornado.web.Application):
         settings = dict(
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
             static_path=os.path.join(os.path.dirname(__file__), "static"),
+            data_path=os.path.join(os.path.dirname(__file__), "data"),
             debug=True,
             )
         super(Application, self).__init__(handlers, **settings)
