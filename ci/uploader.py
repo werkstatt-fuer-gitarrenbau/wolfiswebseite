c3bc02bef07f78e51591467fd5b38fdb94136ac7
Julian Gruber <julian@juliangruber.com>
2025-03-16 15:50:25 +0100

    Revert "Try changing SSL version"
    This reverts commit a576b2f970fa18035f64e80668752509ab595324.

--- ci/uploader.py
+++ ci/uploader.py
@@ -4,7 +4,6 @@ uploads website to ftp server
 
 import os
 import ftplib
-import ssl
 
 
 def render_path(pathlist):
@@ -148,7 +147,6 @@ class FTPClient(object):
     """
     def __init__(self, host, user, password):
         self._ftp = ftplib.FTP_TLS(host, user, password)
-        self._ftp.ssl_version = ssl.PROTOCOL_TLS
         self._dirtree = DirTree(self._ls)
 
     def _ls(self, path=None):
